49e89c2bc1f55ee649747a666f4b42efa36b33e7
Mdot5596 <morganhodge101010@gmail.com>
2025-04-19 12:37:47 +0100

    L1 Boss Model changed , part drop, L2 Gun script altered, no more error due to rigidbody as removed entirely from script

--- Unity/Assets/Scripts/Projectiles/ProjectileGunTutorial.cs
+++ Unity/Assets/Scripts/Projectiles/ProjectileGunTutorial.cs
@@ -1,82 +1,61 @@
-
 using UnityEngine;
 using TMPro;
-using System.Collections.Generic;
 
 public class ProjectileGunTutorial : MonoBehaviour
 {
-
-    //Gun audio
+    // Gun audio
     public AudioSource gunAudio;
 
-    //bullet 
+    // Bullet
     public GameObject bullet;
-    
-    //bullet force
     public float shootForce, upwardForce;
-        
-    // Bullet lifetime
     public float bulletLifetime = 5f;
 
-    //Gun stats
+    // Gun stats
     public float timeBetweenShooting, spread, reloadTime, timeBetweenShots;
     public int magazineSize, bulletsPerTap;
     public bool allowButtonHold;
 
     int bulletsLeft, bulletsShot;
 
-    //Recoil
-    public Rigidbody playerRb;
-    public float recoilForce;
-
-    //bools
+    // Bools
     bool shooting, readyToShoot, reloading;
 
-    //Reference
+    // References
     public Camera fpsCam;
     public Transform attackPoint;
 
-    //Graphics
+    // Graphics
     public GameObject muzzleFlash;
     public TextMeshProUGUI ammunitionDisplay;
 
-
-    //bug fixing 
+    // Invoke control
     public bool allowInvoke = true;
 
     private void Awake()
     {
-        //make sure magazine is full
         bulletsLeft = magazineSize;
         readyToShoot = true;
     }
 
     private void Update()
     {
-
         MyInput();
 
-        //Set ammo display
         if (ammunitionDisplay != null)
             ammunitionDisplay.SetText(bulletsLeft / bulletsPerTap + " / " + magazineSize / bulletsPerTap);
     }
+
     private void MyInput()
     {
-        //Check if allowed to hold down button and take corresponding input
-        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
-        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+        shooting = allowButtonHold ? Input.GetKey(KeyCode.Mouse0) : Input.GetKeyDown(KeyCode.Mouse0);
 
-        //Reloading 
         if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
-        //Reload automatically when trying to shoot without ammo
         if (readyToShoot && shooting && !reloading && bulletsLeft <= 0) Reload();
 
-        //Shooting
         if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
         {
-            //Set bullets shot to 0
             bulletsShot = 0;
-
             Shoot();
         }
     }
@@ -85,67 +64,48 @@ public class ProjectileGunTutorial : MonoBehaviour
     {
         readyToShoot = false;
 
-        //play gunshotsound
         if (gunAudio != null)
-        gunAudio.Play();
+            gunAudio.Play();
 
-        //Find the exact hit position using a raycast
-        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0)); //Just a ray through the middle of your current view
+        Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
+        Vector3 targetPoint = Physics.Raycast(ray, out hit) ? hit.point : ray.GetPoint(75);
 
-        //check if ray hits something
-        Vector3 targetPoint;
-        if (Physics.Raycast(ray, out hit))
-            targetPoint = hit.point;
-        else
-            targetPoint = ray.GetPoint(75); //Just a point far away from the player
-
-        //Calculate direction from attackPoint to targetPoint
         Vector3 directionWithoutSpread = targetPoint - attackPoint.position;
-
-        //Calculate spread
         float x = Random.Range(-spread, spread);
         float y = Random.Range(-spread, spread);
+        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0);
 
-        //Calculate new direction with spread
-        Vector3 directionWithSpread = directionWithoutSpread + new Vector3(x, y, 0); //Just add spread to last direction
-
-        //Instantiate bullet/projectile
-        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity); //store instantiated bullet in currentBullet
-        //Rotate bullet to shoot direction
+        GameObject currentBullet = Instantiate(bullet, attackPoint.position, Quaternion.identity);
         currentBullet.transform.forward = directionWithSpread.normalized;
 
-        //Add forces to bullet
-        currentBullet.GetComponent<Rigidbody>().AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
-        currentBullet.GetComponent<Rigidbody>().AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+        Rigidbody bulletRb = currentBullet.GetComponent<Rigidbody>();
+        if (bulletRb != null)
+        {
+            bulletRb.AddForce(directionWithSpread.normalized * shootForce, ForceMode.Impulse);
+            bulletRb.AddForce(fpsCam.transform.up * upwardForce, ForceMode.Impulse);
+        }
 
-        // Destroy bullet after a lifetime
         Destroy(currentBullet, bulletLifetime);
 
-        //Instantiate muzzle flash, if you have one
         if (muzzleFlash != null)
             Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
 
         bulletsLeft--;
         bulletsShot++;
 
-        //Invoke resetShot function (if not already invoked), with your timeBetweenShooting
         if (allowInvoke)
         {
-            Invoke("ResetShot", timeBetweenShooting);
+            Invoke(nameof(ResetShot), timeBetweenShooting);
             allowInvoke = false;
-
-            //Add recoil to player (should only be called once)
-            playerRb.AddForce(-directionWithSpread.normalized * recoilForce, ForceMode.Impulse);
         }
 
-        //if more than one bulletsPerTap make sure to repeat shoot function
         if (bulletsShot < bulletsPerTap && bulletsLeft > 0)
-            Invoke("Shoot", timeBetweenShots);
+            Invoke(nameof(Shoot), timeBetweenShots);
     }
+
     private void ResetShot()
     {
-        //Allow shooting and invoking again
         readyToShoot = true;
         allowInvoke = true;
     }
@@ -153,12 +113,11 @@ public class ProjectileGunTutorial : MonoBehaviour
     private void Reload()
     {
         reloading = true;
-
-        Invoke("ReloadFinished", reloadTime); //Invoke ReloadFinished function with your reloadTime as delay
+        Invoke(nameof(ReloadFinished), reloadTime);
     }
+
     private void ReloadFinished()
     {
-        //Fill magazine
         bulletsLeft = magazineSize;
         reloading = false;
     }
